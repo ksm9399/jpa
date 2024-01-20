33c5359f88ca8f5ca6a4c5ceb05f61036186a99f
ksm <rkdtjd0909@gmail.com>
2024-01-21 02:49:52 +0900

    상품 리포지토리 개발

---
+++ jpashop/src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,35 @@
+package jpabook.jpashop.repository;
+
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+  private final EntityManager em;
+
+  public void save(Item item) {
+    if (item.getId() == null) {
+      em.persist(item); // 생성 - 인서트
+    }
+    else {
+      em.merge(item); // 수정 - 업데이트
+    }
+  }
+
+  public Item findOne(Long id) {
+    return em.find(Item.class, id);
+  }
+
+  public List<Item> findAll() {
+    return em.createQuery("select i from Item i", Item.class)
+              .getResultList();
+  }
+}
