9c6d3f5b73246d00dab145f8d8974a30f0344e1b
ksm <rkdtjd0909@gmail.com>
2024-01-20 21:10:01 +0900

    회원 서비스 개발

--- jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -5,14 +5,14 @@ import java.util.List;
 import org.springframework.stereotype.Repository;
 
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-  @PersistenceContext
-  private EntityManager em;
+  private final EntityManager em;
 
   public void save(Member member) {
     em.persist(member);
@@ -27,7 +27,7 @@ public class MemberRepository {
               .getResultList();
   }
 
-  public List<Member> findName(String name) {
+  public List<Member> findByName(String name) {
     return em.createQuery("select m from Member m where m.name = :name", Member.class)
               .setParameter("name", name)
               .getResultList();
