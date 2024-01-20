f65eb228bc588df930c2e411995c673e4d723e6c
ksm <rkdtjd0909@gmail.com>
2024-01-21 02:44:08 +0900

    상품 엔티티 개발
    - 비즈니스 로직 추가

--- jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -12,13 +12,14 @@ import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
 import jakarta.persistence.ManyToMany;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
-@Getter @Setter
+@Getter
 public abstract class Item {
 
   @Id @GeneratedValue
@@ -31,4 +32,25 @@ public abstract class Item {
 
   @ManyToMany(mappedBy = "items")
   private List<Category> categories = new ArrayList<>();
+
+  // 비지니스 로직
+  /**
+   * 재고 수량 증가
+   */
+  public void addStock(int quantity) {
+    this.stockQuantity += quantity;
+  }
+
+  /**
+   * 재고 수량 감소
+   */
+  public void removeStock(int quantity) {
+    int restStock = this.stockQuantity - quantity;
+
+    if (restStock < 0) {
+      throw new NotEnoughStockException("need more stock");
+    }
+
+    this.stockQuantity = restStock;
+  }
 }
